38e0fd5d79eda7dd68eaa54a940379f77eda729a
Zachary Cipolletta <ZackCipolletta@gmail.com>
2024-08-20 16:04:26 -0700

    add documentation

--- memory_game/src/App.js
+++ memory_game/src/App.js
@@ -7,26 +7,33 @@ function App() {
   let values = [];
 
   const handleSquareClick = (e) => {
+    // if values array is empty add the value to the array
     if (values.length === 0) {
       changeColor(e);
     } else if (values.length === 1) {
+      // if array is not empty, check value of clicked square against value in array
       verifyValue(e);
     };
   };
 
   const changeColor = (e) => {
+    // if square has been marked 'done' do nothing otherwise chang the square to blue
     if (!e.target.classList.contains('done')) {
       e.target.style.backgroundColor = 'blue';
     };
     addValue(e);
   };
 
+  // if the id of the element in the values array matches the newly clicked square
+  // do nothing, otherwise add it to the array. This prevents a square from matching
+  // to itself.
   const verifyValue = (e) => {
     if (values[0].id !== e.target.id) {
       changeColor(e);
     }
   };
 
+  // add the new value to the array. If there are 2 items in the array, call checkValue function
   const addValue = (e) => {
     values.push(e.target);
     if (values.length === 2) {
@@ -34,22 +41,29 @@ function App() {
     }
   };
 
+
+  // if the data-value of both elements in the array match, change their class name
+  // to 'done' and update background color to green then call resetValues function
   const checkValue = () => {
     if (values[0].getAttribute('data-value') === values[1].getAttribute('data-value')) {
       values.forEach((el) => el.className = `${el.className} done`);
       values.forEach((el) => el.style.backgroundColor = 'green');
       resetValues();
     }
+    // if data-values do not match, wait 1 second then call reset colors
     else {
       setTimeout(resetColors, 1000);
     }
   };
 
+  // reset background colors of each element in array back to original color
+  // then call reset values function
   const resetColors = () => {
     values.forEach((el) => el.style.backgroundColor = 'purple');
     resetValues();
   };
 
+  // reset values to an empty array
   const resetValues = () => {
     values = [];
   };

82aa391fce47841989424c4920ff95e2dfe08340
Zachary Cipolletta <ZackCipolletta@gmail.com>
2024-08-21 17:44:29 -0700

    create function to randomize squares

--- memory_game/src/App.js
+++ memory_game/src/App.js
@@ -69,6 +69,7 @@ function App() {
   };
 
   let col = [];
+  let col2 = [];
 
   (() => {
     for (let i = 1; i <= 25; i++) {
@@ -76,40 +77,20 @@ function App() {
     }
   })();
 
+  function colRandom() {
+    return (Math.floor(Math.random() * col.length))
+  }
+
+  (() => {
+    for (let i = col.length; i > 0; i++) {
+      col2.push(...col.splice(colRandom(), 1));
+    }
+  })();
+
   return (
     <>
-      <div className="grid-container" onClick={handleSquareClick}   >
-        <div id='1' data-value='2' className="grid-item">2</div>
-        <div id='2' data-value='2' className="grid-item">2</div>
-        <div id='3' data-value='4' className="grid-item">4</div>
-        <div id='4' data-value='4' className="grid-item">4</div>
-        <div id='5' data-value='5' className="grid-item">5</div>
-        <div id='6' data-value='6' className="grid-item">6</div>
-        <div id='7' data-value='7' className="grid-item">7</div>
-        <div id='8' data-value='8' className="grid-item">8</div>
-        <div id='9' data-value='9' className="grid-item">9</div>
-        <div id='10' data-value='10' className="grid-item">10</div>
-        <div id='11' data-value='11' className="grid-item">11</div>
-        <div id='12' data-value='12' className="grid-item">12</div>
-        <div id='13' data-value='13' className="grid-item">13</div>
-        <div id='14' data-value='14' className="grid-item">14</div>
-        <div id='15' data-value='15' className="grid-item">15</div>
-        <div id='16' data-value='16' className="grid-item">16</div>
-        <div id='17' data-value='17' className="grid-item">17</div>
-        <div id='18' data-value='18' className="grid-item">18</div>
-        <div id='19' data-value='19' className="grid-item">19</div>
-        <div id='20' data-value='20' className="grid-item">20</div>
-        <div id='21' data-value='21' className="grid-item">21</div>
-        <div id='22' data-value='22' className="grid-item">22</div>
-        <div id='23' data-value='23' className="grid-item">23</div>
-        <div id='24' data-value='24' className="grid-item">24</div>
-        <div id='25' data-value='25' className="grid-item">25</div>
-      </div>
-
-      <p></p>
-
       <div className="grid-container" onClick={handleSquareClick}>
-        {col.map((el, index) => {
+        {col2.map((el, index) => {
           // check if the element in the array is odd or even. If odd add 2, otherwise add 1.
           // assign the resulting value to data-value.
           // This will give each element a matching data-value pair (3 & 3 or 5 & 5, etc)
